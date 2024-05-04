5b063f747d065b936a087828bbb2e47b3a0e6d0f
Eric Cordeiro <lele.eric.santos@gmail.com>
2024-05-04 16:05:36 -0300

    att recovery

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::get('/home', [ProdutoController::class, 'home']);
 Route::get('/cart', [ProdutoController::class, 'cart']);
 Route::get('/dev', [ProdutoController::class, 'dev']);
 Route::get('/ApresProduto', [ProdutoController::class, 'pagProduto']);
-Route::get('/Jogos', [ProdutoController::class, 'allProducts']);
+Route::get('/jogos', [ProdutoController::class, 'allProducts']);
 Route::get('/login', [ ClientController::class, 'login']); 
 Route::get('/cadastro', [ ClientController::class, 'cadastro']); 
