4525da4abfc88abd300de675991c1d3fefa7b08e
Eric Cordeiro <lele.eric.santos@gmail.com>
2024-05-09 15:41:01 -0300

    att nav

--- resources/views/allProducts.blade.php
+++ resources/views/allProducts.blade.php
@@ -4,5 +4,18 @@
     
     @section('content')
 
+    <article>
+      @foreach($produtos as $produto)
+      <div class="col">
+        <div class="card">
+          <img src="{{ asset($produto->PRODUTO_IMAGEM) }}" class="card-img" alt="{{ $produto->PRODUTO_NOME }}">
+          <div class="card-body">
+            <h5 class="card-title">{{ $produto->PRODUTO_NOME }}</h5>
+            <p class="card-text">{{ $produto->PRODUTO_DESC }}</p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </article>
 
     @endsection
