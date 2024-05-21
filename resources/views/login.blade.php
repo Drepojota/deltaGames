af55465d53bf2ec558b524993310bd9dfb9ab4af
Iago Borges Santana <141869310+Sonwcracker@users.noreply.github.com>
2024-05-21 17:45:34 -0300

    css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,38 +1,69 @@
 @extends('layouts.main')
 @section('title', 'Login')
 
+<style>
+    .error-message {
+        color: red;
+        font-size: 0.875em;
+        margin-top: 5px;
+        display: block;
+    }
+    .success-message {
+        color: green;
+        font-size: 0.875em;
+        margin-top: 5px;
+        display: block;
+    }
+
+    .lowhome h2{
+        font-size: 20px;
+        margin-top:50px ;
+        color: aliceblue;
+    }
+
+</style>
+
+
 @section('content')
     <a href="{{ route('homee') }}">Home</a>
     <h2>Login</h2>
-
-    @if(session()->has('success'))
-        <div>{{ session()->get('success') }}</div>
-    @endif
 
     @if (auth()->check())
         <p>Already logged in as {{ auth()->user()->USUARIO_NOME }} | <a href="{{ route('login.destroy') }}">Logout</a></p>
     @else
         <section class="area-login">
             <div class="login">
+                @if(session()->has('success'))
+                    <div class="success-message">{{ session()->get('success') }}</div>
+                @endif
+
                 <div>
                     <img src="{{ asset('image/nav/logo.png') }}" alt="Logo">
                 </div>
 
                 <form action="{{ route('login.store') }}" method="POST">
                     @csrf
-                    <input type="email" name="USUARIO_EMAIL" placeholder="Email de usuário:" required autofocus>
-                    @error('USUARIO_EMAIL')
-                        <span>{{ $message }}</span>
-                    @enderror
+  
+                        <input type="email" name="USUARIO_EMAIL" placeholder="Email de usuário:" required autofocus value="{{ old('USUARIO_EMAIL') }}">
+                        @error('USUARIO_EMAIL')
+                            <span class="error-message">{{ $message }}</span>
+                        @enderror
 
-                    <input type="password" name="USUARIO_SENHA" placeholder="Sua senha:" required>
-                    @error('USUARIO_SENHA')
-                        <span>{{ $message }}</span>
-                    @enderror
+                
+                        <input type="password" name="USUARIO_SENHA" placeholder="Sua senha:" required>
+                        @error('USUARIO_SENHA')
+                            <span class="error-message">{{ $message }}</span>
+                        @enderror
+                    
                     <input type="submit" value="Entrar">
                 </form>
 
                 <p>Ainda não tem uma conta? <a href="{{ route('register.show') }}">Criar Conta</a></p>
+
+                <div class="lowhome">
+                    <h2>Login <a href="{{ route('homee') }}">Home</a></h2>
+                </div>
+
             </div>
         </section>
     @endif
