29f0b79edc422183cc06760f32cffd08c8e56d8c
Eric Cordeiro <lele.eric.santos@gmail.com>
2024-05-26 01:44:54 -0300

    att

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,6 +23,9 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
 
+    <?php
+    use Illuminate\Support\Facades\Auth;
+    ?>
 </head>
 
 <body>
