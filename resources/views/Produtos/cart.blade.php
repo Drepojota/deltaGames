4f9572496a1cbca518b2b07c1d3aff2670f5aa80
João Pedro <143830987+Drepojota@users.noreply.github.com>
2024-06-04 01:20:21 -0300

    Att. MarketCar

--- resources/views/Produtos/cart.blade.php
+++ resources/views/Produtos/cart.blade.php
@@ -77,6 +77,9 @@
                     </form>
                 </div>
             </div>
+
+         
+
         </aside>
     </div>
 </main>
@@ -113,11 +116,10 @@
                         </p>
                     </div>
                 </div>
-            </a>
+            </a>    
         </div>
         @endforeach
     </div>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 @endsection
