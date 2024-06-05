93768170ee6994231256a2082354a3e8736256bd
Iago Borges Santana <141869310+Sonwcracker@users.noreply.github.com>
2024-06-05 08:21:48 -0300

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+
 class LoginController extends Controller
 {
     public function index()
